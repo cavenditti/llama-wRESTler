5887d7f07d3901476c246f8fa462d3e9b01b4759
Carlo Antonio Venditti <c.a.venditti@pm.me>
2025-12-03 00:15:34 +0100

    refactor: formatting

--- llama_wrestler/phases/__init__.py
+++ llama_wrestler/phases/__init__.py
@@ -1,6 +1,12 @@
 from llama_wrestler.phases.preliminary import run_preliminary_phase, PreliminaryResult
-from llama_wrestler.phases.data_generation import run_data_generation_phase, GeneratedTestData
-from llama_wrestler.phases.test_execution import run_test_execution_phase, APIExecutionResult
+from llama_wrestler.phases.data_generation import (
+    run_data_generation_phase,
+    GeneratedTestData,
+)
+from llama_wrestler.phases.test_execution import (
+    run_test_execution_phase,
+    APIExecutionResult,
+)
 
 __all__ = [
     "run_preliminary_phase",
